1e047b0e47e1a67a81f52117cdb409289e55364e
zodplugin <zodplugin@gmail.com>
2026-05-28 13:25:29 +0700

    feat: time off request

---
+++ app/Models/TimeOffRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TimeOffRequest extends Model
+{
+    protected $fillable = [
+        'employee_id',
+        'type',
+        'start_date',
+        'end_date',
+        'duration',
+        'reason',
+        'status',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
