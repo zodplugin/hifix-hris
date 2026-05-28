fbe4fd1ab831376c42f0b23d75dbffb6b4d2acac
zodplugin <zodplugin@gmail.com>
2026-05-28 13:25:57 +0700

    feat: department factory

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Department>
+ */
+class DepartmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->jobTitle(),
+            'code' => fake()->unique()->lexify('???'),
+            'manager_id' => null, // Set in seeder
+        ];
+    }
+}
